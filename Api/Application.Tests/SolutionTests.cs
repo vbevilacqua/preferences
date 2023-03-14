5aa6feb4b8c188a875581dccac9e6553de86104f
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-03-14 18:00:45 +0100

    Move Auth0 classes to Infra and remove unnecessary dependencies

--- Api/Application.Tests/SolutionTests.cs
+++ Api/Application.Tests/SolutionTests.cs
@@ -1,3 +1,8 @@
+using System.Net.Http.Headers;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Microsoft.Extensions.Configuration;
+
 namespace Api.Tests
 {
     using Application.Solutions.Commands;
@@ -15,11 +20,35 @@ namespace Api.Tests
     [TestCaseOrderer("Api.Tests.Helpers.PriorityOrderer", "Api.Tests")]
     public class SolutionTests : IClassFixture<PreferencesWebApplicationFactory<Program>>
     {
+        
+        private readonly HttpClient httpClient;
+        private readonly IConfigurationSection auth0Settings;
         private PreferencesWebApplicationFactory<Program> _factory;
 
         public SolutionTests(PreferencesWebApplicationFactory<Program> factory)
         {
             _factory = factory;
+            httpClient = factory.CreateClient();
+
+            auth0Settings = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetSection("Auth0");
+        }
+        
+        async Task<string> GetAccessToken()
+        {
+            var auth0Client = new AuthenticationApiClient(auth0Settings["Domain"]);
+            var tokenRequest = new ClientCredentialsTokenRequest()
+            {
+                ClientId = auth0Settings["ClientId"],
+                ClientSecret = auth0Settings["ClientSecret"],
+                Audience = auth0Settings["Audience"]
+            };
+            var tokenResponse = await auth0Client.GetTokenAsync(tokenRequest);
+
+            return tokenResponse.AccessToken;
         }
 
         [Fact, TestPriority(1)]
